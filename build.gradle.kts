cb7b1775d1eb4df2fc654367e812d800fd960463
intern01 <intern2026-nakazono@i-system.co.jp>
2026-08-24 11:24:44 +0900

    Supabaseを使用し、メールアドレスでアカウント登録、ユーザー名、パスワード、プロフィール写真を保持するよう変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    //追加箇所
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
 }
